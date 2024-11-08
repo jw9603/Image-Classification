709f9522033cae8a7458ef586a204c9e402d135b
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-11-08 16:50:43 +0900

    Update run.sh

--- mnist_classification/run.sh
+++ mnist_classification/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# 현재 날짜와 시간을 파일명에 추가하여 저장
+# Save the current date and time to add to the filename
 dt=`date '+%Y%m%d_%H%M%S'`
 
-# 결과 모델을 저장할 디렉토리 생성
+# Create a directory to store the resulting model
 mkdir -p ./result_model
 
-# 매개변수 설정
-bs=64                # 배치 크기
-epochs=30            # 에포크 수
-train_ratio=0.8      # 학습 데이터 비율
+# Set parameters
+bs=64                # Batch size
+epochs=30            # Number of epochs
+train_ratio=0.8      # Training data ratio
 
-# Python 스크립트 실행
+# Run the Python script
 python train.py \
     --model "./result_model/mnist_classification_${dt}_bs${bs}_epoch${epochs}_ratio${train_ratio}.pth" \
     --batch_size $bs \
