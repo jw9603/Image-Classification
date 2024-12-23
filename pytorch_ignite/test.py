370e8c6b3316c774da9e5e2dda0f138116b2a832
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-23 22:16:02 +0900

    LSTM 테스팅 추가

--- pytorch_ignite/test.py
+++ pytorch_ignite/test.py
@@ -4,6 +4,8 @@ import torch.nn
 import sys
 from model.fc_model import FcClassifier
 from model.cnn_model import ConvolutionalClassifier
+from model.rnn_model import LSTMClassifier
+from data_loader import load_mnist
 import os
 
 
@@ -37,18 +39,41 @@ def test(model, x, y):
 if __name__ == '__main__':
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model_list = get_model_list('./result_model/')
+       
+
+    ## Before
+    # # model = FcClassifier(28**2, 10).to(device)
+    # model = ConvolutionalClassifier(10).to(device)
+    # accuracy = []
+    # for i in model_list:
+    #     model.load_state_dict(load(fn=i, device=device))
+    #     acc = test(model=model, x=x, y=y)
+    #     accuracy.append((i,acc))
     
-    x, y = load_mnist(is_train=False, flatten=True)
-    
-    x, y = x.to(device), y.to(device)    
-    
-    
-    # model = FcClassifier(28**2, 10).to(device)
-    model = ConvolutionalClassifier(10).to(device)
+    # Update Version
     accuracy = []
-    for i in model_list:
-        model.load_state_dict(load(fn=i, device=device))
+    for model_path in model_list:
+        
+        if 'fc' in model_path.lower():
+            model = FcClassifier(28**2, 10).to(device)
+            x, y = load_mnist(is_train=False, flatten=True)
+            x, y = x.to(device), y.to(device) 
+            
+        elif 'cnn' in model_path.lower():
+            x, y = load_mnist(is_train=False, flatten=False)
+            x, y = x.to(device), y.to(device) 
+            model = ConvolutionalClassifier(10).to(device)
+        elif 'rnn' in model_path.lower():
+            x, y = load_mnist(is_train=False, flatten=False)
+            x, y = x.to(device), y.to(device)
+            # run_train.sh 파일에서 매개변수들을 확인
+            model = LSTMClassifier(input_size=28, hidden_size=128, output_size=10, n_layers=4, dropout_p=0.2)
+        else:
+            print(f"Unknown model type in file :{model_path}")
+            continue
+        
+        model.load_state_dict(load(fn=model_path, device=device))
         acc = test(model=model, x=x, y=y)
-        accuracy.append((i,acc))
+        accuracy.append((model_path, acc))
         
     print('\n'.join([f"{i}: {j:.4f}" for i, j in accuracy]))
