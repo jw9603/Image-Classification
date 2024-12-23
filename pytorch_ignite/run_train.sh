e56159c95fe155729537682002e641ea77437ee9
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-23 21:48:52 +0900

    Update run_train.sh - rnn 추가로 인한 업데이트

--- pytorch_ignite/run_train.sh
+++ pytorch_ignite/run_train.sh
@@ -8,14 +8,30 @@ mkdir -p ./result_model
 
 # Set parameters
 bs=256               # Batch size
-epochs=5             # Number of epochs
+epochs=3             # Number of epochs
 train_ratio=0.8      # Training data ratio
-model_name='cnn'     # fc or cnn
+model_name='rnn'     # fc, cnn, and rnn
 
-# Run the Python script
-python train.py \
-    --model_file "./result_model/mnist_classification_${model_name}_${dt}_bs${bs}_epoch${epochs}_ratio${train_ratio}.pth" \
+# RNN-specific parameters
+hidden_size=128
+n_layers=4
+dropout_p=0.2
+
+# Construct the Python command
+python_command="python train.py \
+    --model_file \"./result_model/mnist_classification_${model_name}_${dt}_bs${bs}_epoch${epochs}_ratio${train_ratio}.pth\" \
     --model $model_name \
     --batch_size $bs \
     --n_epochs $epochs \
-    --train_ratio $train_ratio
+    --train_ratio $train_ratio"
+
+# Add RNN-specific arguments if model_name is 'rnn'
+if [ "$model_name" == "rnn" ]; then
+    python_command+=" \
+    --hidden_size $hidden_size \
+    --n_layers $n_layers \
+    --dropout_p $dropout_p"
+fi
+
+# Execute the Python command
+eval $python_command
